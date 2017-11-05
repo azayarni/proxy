faf8ec4561724edce77df95ab5f201dfa44139af
azayarni <dev@zayarni.net>
2017-11-05 14:38:05 +0100

    fetcher

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    if (!req.query.u) res.send('hello');
+
+    const opts = {
+        url: req.query.u,
+        headers : {
+            "User-Agent" : "Mozilla/5.0 (compatible; rogerBot/1.0; UrlCrawler;)"
+        }
+    };
+
+    request(opts, (error, response, body) => {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        res.send(body); // Print the HTML for the Google homepage.
+    });
+});
+
+app.listen(3200, () => console.log('Example app listening on port 3200!'))
